6e05e0676b36a076b4a034b6c1c498ee56a69128
Jose Candido <candidofilho@gmail.com>
2022-06-24 14:36:15 -0300

    fixing css

--- memurb/src/routes/home.svelte
+++ memurb/src/routes/home.svelte
@@ -1,10 +1,23 @@
 <h2 class="font-julius">Cidades</h2>
 <ul>
-    <li class="city shadow"><a href="/cities/fortaleza.html">Fortaleza</a></li>
+    <li class="city shadow {foundAll ? 'found' : ''}"><a href="/cities/fortaleza.html">Fortaleza</a></li>
 </ul>
 
 <script lang="ts">
     import { showHeaderFooter } from '../state/showHeaderFooter';
+    import { onMount } from 'svelte';
     showHeaderFooter.update(value => ({ show:false, path: "/home.html", text: "Home"}));
 
+    let foundAll = false;
+
+    onMount(() => {
+        var profile = JSON.parse(localStorage.getItem("MEMURB_PROFILE") ?? "{}");
+
+        var places = profile.places;
+
+        if(places)
+        foundAll = !places.find(p => !p.found);
+
+
+    });
 </script>
