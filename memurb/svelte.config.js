70e112dd1eeee66511070d92b2bc5e34c8b67ff6
Jose Candido <jcqlfh@gmail.com>
2023-12-09 14:47:48 -0300

    updating trailingslash

--- memurb/svelte.config.js
+++ memurb/svelte.config.js
@@ -1,24 +1,23 @@
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-import { mdsvex } from 'mdsvex'
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
-    extensions: ['.svelte', '.md'],
-    preprocess: [
-        mdsvex({
-            extensions: ['.md']
-        }),
-        preprocess(),
-    ],
-    kit: {
-        prerender: {
-            default: true,
-        },
-        adapter: adapter({ trailingSlash: 'always' }),
-
-    },
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		mdsvex({
+			extensions: ['.md']
+		}),
+		preprocess()
+	],
+	kit: {
+		prerender: {
+			default: true
+		},
+		adapter: adapter()
+	}
 };
 export default config;
